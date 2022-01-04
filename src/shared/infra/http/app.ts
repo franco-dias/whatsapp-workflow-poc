923d0562d679bfabcf13b0b3655a9eb0f2752067
franco-dias <gabrielfdg10@gmail.com>
2022-01-04 16:49:40 -0300

    fix: cleanup

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,31 +1,20 @@
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { config } from 'dotenv';
 import express, { Express } from 'express';
 import 'express-async-errors';
-import { resolve } from 'path';
 
-import { errorHandler, NotFoundError } from '@imobo/nano-common';
+import { AppError } from '@shared/errors/AppError';
 
 import { database } from '../typeorm';
+import errorHandler from './middlewares/errorHandler';
 import routes from './routes/v2';
 
 export class App {
   server: Express;
 
-  constructor() {
-    this.environment();
-  }
-
   setup() {
     this.server = express();
     this.middlewares();
     this.routes();
-  }
-
-  environment() {
-    config({ path: resolve(__dirname, '..', '..', '..', '..', '.env.local') });
-    config({ path: resolve(__dirname, '..', '..', '..', '..', '.env') });
   }
 
   async connections() {
@@ -39,20 +28,14 @@ export class App {
   middlewares() {
     this.server.set('trust proxy', true);
     this.server.use(express.json());
-    this.server.use(cookieParser());
     this.server.use(cors({ credentials: true, origin: '*' }));
   }
 
   routes() {
-    this.server.use(
-      '/auth/doc',
-      express.static(resolve(__dirname, '..', '..', '..', 'swagger'))
-    );
-
     this.server.use('/auth/v1', routes);
     this.server.all('*', async (req) => {
       console.log(`[APP] Request Not Found: ${req.method} - ${req.url}`);
-      throw new NotFoundError();
+      throw new AppError('Not found', 404);
     });
     this.server.use(errorHandler);
   }
